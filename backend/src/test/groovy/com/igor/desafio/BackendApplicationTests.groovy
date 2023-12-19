119354a35384222605a429d4824b98f3a35f54fd
igorpadua <igorpadua@tutanota.com>
2023-12-19 14:56:47 -0300

    backend init

---
+++ backend/src/test/groovy/com/igor/desafio/BackendApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.igor.desafio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

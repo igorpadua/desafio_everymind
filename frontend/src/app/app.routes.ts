65fc54d48a5df0cd852dec0dec7759c40f9d8034
igorpadua <igorpadua@tutanota.com>
2023-12-20 09:33:59 -0300

    editar produto

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {ProdutoReadComponent} from "./components/produto-read/produto-read.component";
 import {ProdutoCreateComponent} from "./components/produto-create/produto-create.component";
+import {ProdutoUpdateComponent} from "./components/produto-update/produto-update.component";
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'novo',
     component: ProdutoCreateComponent
+  },
+  {
+    path: 'editar/:id',
+    component: ProdutoUpdateComponent
   }
 ];
---
+++ frontend/src/app/components/produto-update/produto-update.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {Produto} from "../../model/produto.model";
+import {ProdutoService} from "../../service/produto.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-produto-update',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './produto-update.component.html',
+  styleUrl: './produto-update.component.css'
+})
+export class ProdutoUpdateComponent implements OnInit {
+
+  produto: Produto = {
+    id: 0,
+    nome: '',
+    codigo: 0,
+    descricao: '',
+    preco: 0.0,
+  }
+
+  constructor(private produtoService: ProdutoService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id: number = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.produtoService.readById(id).subscribe(produto => {
+        this.produto = produto;
+      })
+    }
+  }
+
+  updateProduto() {
+    this.produtoService.update(this.produto).subscribe(() => {
+      alert('Produto atualizado com sucesso!')
+    })
+    this.router.navigate(['/']);
+  }
+}

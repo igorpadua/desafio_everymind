b0d18a89bcb02ab12788dae61844c163941ba522
igorpadua <igorpadua@tutanota.com>
2023-12-20 09:26:21 -0300

    criar produto

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {ProdutoReadComponent} from "./components/produto-read/produto-read.component";
+import {ProdutoCreateComponent} from "./components/produto-create/produto-create.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProdutoReadComponent
+  },
+  {
+    path: 'novo',
+    component: ProdutoCreateComponent
+  }
+];
---
+++ frontend/src/app/components/produto-create/produto-create.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {Produto} from "../../model/produto.model";
+import {Router} from "@angular/router";
+import {ProdutoService} from "../../service/produto.service";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-produto-create',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './produto-create.component.html',
+  styleUrl: './produto-create.component.css'
+})
+export class ProdutoCreateComponent {
+
+  produto: Produto = {
+    id: 0,
+    nome: '',
+    codigo: 0,
+    descricao: '',
+    preco: 0.0,
+  }
+
+  constructor(private router: Router, private produtoService: ProdutoService) { }
+
+  criarProduto() {
+    this.produtoService.create(this.produto).subscribe(() => {
+      alert('Produto criado com sucesso!')
+    })
+    this.router.navigate(['/']);
+  }
+}
